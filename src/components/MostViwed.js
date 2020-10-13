ab428bb62af7f12aa4d2b035ffe2074795212cae
sarra <saraalsara@hotmail.com>
2020-10-13 21:53:10 +0300

    create helper file to add array methods

--- src/components/MostViwed.js
+++ src/components/MostViwed.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import Movie from './Movie'
 import MovieGrid from './MovieGrid'
+import { mostViwed } from '../utils/helper'
 
 class MostViwed extends React.Component {
   render() {
-    const sortData = this.props.movies.sort((movie1, movie2) => {
-      return movie1.popularity > movie2.popularity ? -1 : 1
-    });
     return (
-      <MovieGrid gridType="is-suggested" title="الأكثر مشاهدة">
-        <Movie title={sortData[4].originalTitle} image={sortData[4].posterPath} />
-        <Movie title={sortData[5].originalTitle} image={sortData[5].posterPath} />
-        <Movie title={sortData[6].originalTitle} image={sortData[6].posterPath} />
-        <Movie title={sortData[7].originalTitle} image={sortData[7].posterPath} />
-      </MovieGrid>
+      <MovieGrid
+        gridType="is-suggested"
+        title="الأكثر مشاهدة"
+        movies={mostViwed([...this.props.movies])}
+        Limit={4}
+      />
     );
   }
 }
--- src/components/NewMovies.js
+++ src/components/NewMovies.js
@@ -1,28 +1,15 @@
 import React from 'react';
-import Movie from './Movie';
 import MovieGrid from './MovieGrid'
+import { recentRelease } from '../utils/helper'
 
 class NewMovies extends React.Component {
   render() {
-    const filterMovies = this.props.movies.filter(movie => {
-      return movie.category === "Film"
-    })
-    const sortFilterMovies = filterMovies.sort((movie1, movie2) => {
-      return new Date(movie1.releaseDate) > new Date(movie2.releaseDate) ? -1 : 1
-    })
-    console.log(filterMovies, 22)
-    console.log(sortFilterMovies, 22)
     return (
-      <MovieGrid gridType="is-movies" title="أحدث الأفلام " >
-        <Movie title={sortFilterMovies[16].originalTitle} image={sortFilterMovies[16].posterPath} />
-        <Movie title={sortFilterMovies[17].originalTitle} image={sortFilterMovies[17].posterPath} />
-        <Movie title={sortFilterMovies[18].originalTitle} image={sortFilterMovies[18].posterPath} />
-        <Movie title={sortFilterMovies[19].originalTitle} image={sortFilterMovies[19].posterPath} />
-        <Movie title={sortFilterMovies[20].originalTitle} image={sortFilterMovies[20].posterPath} />
-        <Movie title={sortFilterMovies[21].originalTitle} image={sortFilterMovies[21].posterPath} />
-        <Movie title={sortFilterMovies[22].originalTitle} image={sortFilterMovies[22].posterPath} />
-        <Movie title={sortFilterMovies[23].originalTitle} image={sortFilterMovies[23].posterPath} />
-      </MovieGrid>
+      <MovieGrid
+        gridType="is-movies"
+        title="أحدث الأفلام "
+        movies={recentRelease(this.props.movies, 'Film')}
+        Limit={8} />
     );
   }
 }
